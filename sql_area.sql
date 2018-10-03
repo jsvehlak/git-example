624cc5d982ec84a54c29317df7907110952ed7db
jsvehlak <jaroslav.svehlak@seznam.cz>
2018-10-03 23:28:10 +0200

    ANSI

--- sql_area.sql
+++ sql_area.sql
@@ -9,12 +9,9 @@ SELECT s.sid,
        s.osuser "OS User",
        Substr(a.sql_text,1,120) "SQL Text",
        s.program "Program"
-FROM   v$session s,
-       v$sqlarea a,
-       v$process p
-WHERE  s.sql_hash_value = a.hash_value (+)
-AND    s.sql_address    = a.address (+)
-AND    s.paddr          = p.addr;
-
+FROM   v$session s 
+       left outer join v$sqlarea a ON s.sql_hash_value = a.hash_value and s.sql_address    = a.address
+       LEFT JOIN v$process p on s.paddr= p.addr;
+       
 SET PAGESIZE 14
 SET FEEDBACK ON
